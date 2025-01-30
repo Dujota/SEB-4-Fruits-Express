4fd922b9989e4fd04052ac5676e07bff98493060
Denis S Dujota <denisdujota@gmail.com>
2025-01-30 14:11:19 +0300

    complete up to delete

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+/* eslint-disable no-console */
 /* eslint-disable object-shorthand */
 require('dotenv').config();
 require('./config/database');
 const express = require('express');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 
 const app = express();
 // MODELS
 const Fruit = require('./models/fruit');
 
 // MIDDLEWARE
+app.use(methodOverride('_method'));
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 
@@ -43,9 +46,23 @@ app.post('/fruits', async (req, res) => {
   } catch (err) {
     console.log('ERROR:', err);
   }
-  res.redirect('/fruits/new');
+  res.redirect('/fruits');
   console.log(req.body);
 });
+
+// Fruit Show
+app.get('/fruits/:fruitId', async (req, res) => {
+  const fruit = await Fruit.findById(req.params.fruitId);
+  res.render('fruits/show.ejs', { fruit: fruit });
+});
+
+// Fruit Delete
+app.delete('/fruits/:fruitId', async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitId);
+  res.redirect('/fruits');
+});
+
+// Fruit update
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port 3000');
